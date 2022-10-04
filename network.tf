fa4890c882212db5fc0133c0f358fa8be0878411
Maciej Groszyk <maciej.groszyk8@gmail.com>
2022-10-04 14:29:13 +0200

    fix tags

--- network.tf
+++ network.tf
@@ -1,6 +1,6 @@
 resource "aws_internet_gateway" "maciejgroszyk_tf_igw" {
     vpc_id = "${aws_vpc.maciejgroszyk_tf_vpc.id}"
-    tags {
+    tags = {
 	Name = "maciejgroszyk_tf_igw"
     }
 }
@@ -11,14 +11,14 @@ resource "aws_route_table" "maciejgroszyk_tf_crt" {
 	cidr_block = "0.0.0.0/0"
   	gateway_id = "{aws_internet_gateway.maciejgroszyk_tf_igw.id}"
     }
-    tags {
+    tags = {
 	Name = "maciejgroszyk_tf_crt"
     }
 }
 
 resource "aws_route_table_association" "mg_crta_public_subnet-1" {
     subnet_id = "${aws_subnet.mg-subnet-public-1.id}"
-    route_table_id = "${aws_route_table.maciejgroszyl_tf_crt.id}"
+    route_table_id = "${aws_route_table.maciejgroszyk_tf_crt.id}"
 }
 
 resource "aws_security_group" "mg_security_group_tf" {
