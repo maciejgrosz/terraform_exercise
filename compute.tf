ec94464b4381cf831adf5309b0329bd2d8b24dd9
Maciej Groszyk <maciej.groszyk8@gmail.com>
2022-10-04 17:01:50 +0200

    add alb

--- compute.tf
+++ compute.tf
@@ -15,3 +15,21 @@ resource "aws_instance" "maciejgroszyk_tf_ec2" {
     vpc_security_group_ids = [aws_security_group.mg_security_group_tf.id]
     key_name = "londonmaciejgroszyk"
 }
+
+resource "aws_instance" "maciejgroszyk_tf_ec2-2" {
+    ami           = "ami-0f540e9f488cfa27d"
+    instance_type = "t2.micro"
+    subnet_id = "${aws_subnet.maciejgroszyk_tf-public-subnet-1.id}"
+    tags = {
+        Name = "maciejgroszyk_tf_ec2"
+        created_by = "Maciej Groszyk"
+        bootcamp = "poland1"
+    }
+
+    volume_tags = {
+        created_by = "Maciej Groszyk"
+        bootcamp = "poland1"
+    }
+    vpc_security_group_ids = [aws_security_group.mg_security_group_tf.id]
+    key_name = "londonmaciejgroszyk"
+}
--- network.tf
+++ network.tf
@@ -5,21 +5,21 @@ resource "aws_internet_gateway" "maciejgroszyk_tf_igw" {
     }
 }
 
-resource "aws_route_table" "maciejgroszyk_tf_crt" {
-    vpc_id = "${aws_vpc.maciejgroszyk_tf_vpc.id}"
-    route {
-	cidr_block = "0.0.0.0/0"
-  	gateway_id = "${aws_internet_gateway.maciejgroszyk_tf_igw.id}"
-    }
-    tags = {
-	Name = "maciejgroszyk_tf_crt"
-    }
-}
+# resource "aws_route_table" "maciejgroszyk_tf_crt" {
+#     vpc_id = "${aws_vpc.maciejgroszyk_tf_vpc.id}"
+#     route {
+# 	cidr_block = "0.0.0.0/0"
+#   	gateway_id = "${aws_internet_gateway.maciejgroszyk_tf_igw.id}"
+#     }
+#     tags = {
+# 	Name = "maciejgroszyk_tf_crt"
+#     }
+# }
 
-resource "aws_route_table_association" "mg_crta_public_subnet-1" {
-    subnet_id = "${aws_subnet.maciejgroszyk_tf-public-subnet-1.id}"
-    route_table_id = "${aws_route_table.maciejgroszyk_tf_crt.id}"
-}
+# resource "aws_route_table_association" "mg_crta_public_subnet-1" {
+#     subnet_id = "${aws_subnet.maciejgroszyk_tf-public-subnet-1.id}"
+#     route_table_id = "${aws_route_table.maciejgroszyk_tf_crt.id}"
+# }
 
 resource "aws_security_group" "mg_security_group_tf" {
     vpc_id = "${aws_vpc.maciejgroszyk_tf_vpc.id}"
