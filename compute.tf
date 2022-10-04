af3d65528f9918ba2584f01eebd77ddfa1e552ac
Maciej Groszyk <maciej.groszyk8@gmail.com>
2022-10-04 14:41:38 +0200

    fix tags

--- compute.tf
+++ compute.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "maciejgroszyk_tf_ec2" {
     ami           = "ami-0f540e9f488cfa27d"
     instance_type = "t2.micro"
-    subnet_id     = data.aws_subnet.maciejgroszyk_tf-public-subnet-1.id
+    subnet_id = "${aws_subnet.maciejgroszyk_tf-public-subnet-1.id}"
     tags = {
     Name = "maciejgroszyk_tf_ec2"
     }
--- data.tf
+++
@@ -1,5 +0,0 @@
-data "aws_subnet" "maciejgroszyk_tf-public-subnet-1" {
-    tags = {
-        Name = "maciejgroszyk_tf-public-subnet-1"
-    }
-}
