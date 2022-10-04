912abbb20e58f0e60b0ea0e0ae97382adadbcd68
Maciej Groszyk <maciej.groszyk8@gmail.com>
2022-10-04 14:48:51 +0200

    fix

--- subnet.tf
+++ subnet.tf
@@ -1,5 +1,6 @@
 resource "aws_subnet" "maciejgroszyk_tf-public-subnet-1" {
     vpc_id = "${aws_vpc.maciejgroszyk_tf_vpc.id}"
+    cidr_block = "10.0.0.0/24"
     map_public_ip_on_launch = "true"
     availability_zone = "eu-west-2a"
     tags = {
