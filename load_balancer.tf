dc3392de1e38e9ca3b6fc5d37a15629a88a03e70
Maciej Groszyk <maciej.groszyk8@gmail.com>
2022-10-06 01:51:20 +0200

    add locals

--- load_balancer.tf
+++ load_balancer.tf
@@ -47,6 +47,11 @@ resource "aws_lb_listener" "alb" {
   load_balancer_arn = aws_lb.alb.arn
   port              = "80"
   protocol          = "HTTP"
+  
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.mg-tf-target_group.arn
+  }
 }
 
 resource "aws_lb_target_group" "mg-tf-target_group" {
