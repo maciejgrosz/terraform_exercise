626d4dcf958fed1c9afba09b834e25eb60224020
Maciej Groszyk <maciej.groszyk8@gmail.com>
2022-10-05 14:21:30 +0200

    add for each

--- compute.tf
+++ compute.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "maciejgroszyk_tf_ec2" {
   for_each               = var.aws_instances
   ami                    = each.value.ami
   instance_type          = each.value.instance_type
-  subnet_id              = aws_subnet.maciejgroszyk_tf-public-subnet-1.id
+  subnet_id              = aws_subnet.subnets["a"].id
   tags                   = each.value.tags
   user_data              = file(each.value.start_script)
   volume_tags            = var.volume_tags
