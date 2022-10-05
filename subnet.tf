8a6493ba95b7bea7bf77e21819f32947782137c1
Maciej Groszyk <maciej.groszyk8@gmail.com>
2022-10-05 15:19:50 +0200

    add for each

--- subnet.tf
+++ subnet.tf
@@ -4,4 +4,5 @@ resource "aws_subnet" "subnets" {
   cidr_block        = each.value.cidr
   availability_zone = each.value.az
   tags              = each.value.tags
+  map_public_ip_on_launch = "true"
 }
