7cfef6d138dd2e78adab5eebe3666fd3c476716f
Maciej Groszyk <maciej.groszyk8@gmail.com>
2022-10-06 02:12:00 +0200

    add locals

--- vpc.tf
+++ vpc.tf
@@ -1,7 +1,7 @@
 resource "aws_vpc" "maciejgroszyk_tf_vpc" {
-  cidr_block           = var.tf_vpc[cidr_block]
-  enable_dns_support   = var.tf_vpc[enable_dns_support]
-  enable_dns_hostnames = var.tf_vpc[enable_dns_hostnames]
-  instance_tenancy     = var.tf_vpc[instance_tenancy]
-  tags                 = var.tf_vpc[tags]
+  cidr_block           = var.tf_vpc["cidr_block"]
+  enable_dns_support   = var.tf_vpc["enable_dns_support"]
+  enable_dns_hostnames = var.tf_vpc["enable_dns_hostnames"]
+  instance_tenancy     = var.tf_vpc["instance_tenancy"]
+  tags                 = var.tf_vpc["tags"]
 }
