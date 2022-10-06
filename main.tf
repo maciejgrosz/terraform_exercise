93e2b86058252b577dcf4309c31e9ff279af9cd1
Maciej Groszyk <maciej.groszyk8@gmail.com>
2022-10-06 17:59:21 +0200

    add health check vars

--- main.tf
+++ main.tf
@@ -5,15 +5,17 @@ module "compute" {
   source        = "./modules/compute"
   aws_instances = var.aws_instances
 
-  subnets_list      = module.network.subnets_list
-  vpc_id            = module.network.vpc_id
-  security_group_id = module.network.security_group_id
-  lb_name           = var.lb_name
-  lb_type           = var.lb_type
-  mg_target_group   = var.mg_target_group
-  attach_port       = var.attach_port
-  volume_tags       = var.volume_tags
-  alb_listener      = var.alb_listener
+  subnets_list          = module.network.subnets_list
+  vpc_id                = module.network.vpc_id
+  security_group_id     = module.network.security_group_id
+  lb_name               = var.lb_name
+  lb_type               = var.lb_type
+  mg_target_group       = var.mg_target_group
+  attach_port           = var.attach_port
+  volume_tags           = var.volume_tags
+  alb_listener          = var.alb_listener
+  health_check_interval = var.health_check_interval
+  health_check_timeout  = var.health_check_timeout
 }
 
 module "network" {
