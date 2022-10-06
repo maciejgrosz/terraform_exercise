030526767ef2cbdc0275f9d26b893eac9dea8e95
Maciej Groszyk <maciej.groszyk8@gmail.com>
2022-10-06 02:13:06 +0200

    add locals

--- load_balancer.tf
+++ load_balancer.tf
@@ -7,24 +7,24 @@ resource "aws_lb" "alb" {
 
 resource "aws_lb_listener" "alb" {
   load_balancer_arn = aws_lb.alb.arn
-  port              = var.alb_listener[port]
-  protocol          = var.alb_listener[protocol]
+  port              = var.alb_listener["port"]
+  protocol          = var.alb_listener["protocol"]
 
   default_action {
-    type             = var.alb_listener[default_action_type]
+    type             = var.alb_listener["default_action_type"]
     target_group_arn = aws_lb_target_group.mg-tf-target_group.arn
   }
 }
 
 resource "aws_lb_target_group" "mg-tf-target_group" {
-  name     = var.mg_target_group[name]
-  port     = var.mg_target_group[port]
-  protocol = var.mg_target_group[protocol]
+  name     = var.mg_target_group["name"]
+  port     = var.mg_target_group["port"]
+  protocol = var.mg_target_group["protocol"]
   vpc_id   = aws_vpc.maciejgroszyk_tf_vpc.id
 
   health_check {
-    port     = var.mg_target_group[port]
-    protocol = var.mg_target_group[protocol]
+    port     = var.mg_target_group["port"]
+    protocol = var.mg_target_group["protocol"]
     timeout  = 5
     interval = 10
   }
