c14da92552e74eaf1ac2327eaba44577ebd836ed
Maciej Groszyk <maciej.groszyk8@gmail.com>
2022-10-06 14:40:04 +0200

    add modules

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ module "compute" {
     source = "./modules/compute"
     aws_instances = var.aws_instances
 
-    subnets = [module.network.subnet_a_id, module.network.subnet_b_id]
+    subnets_list = [module.network.subnet_a_id, module.network.subnet_b_id]
 }
 
 module "network" {
