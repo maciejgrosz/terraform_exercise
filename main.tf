bb4d87bbb601d8508c5ad103014d2deb85128c18
Maciej Groszyk <maciej.groszyk8@gmail.com>
2022-10-06 17:52:19 +0200

    add modules

--- main.tf
+++ main.tf
@@ -19,5 +19,13 @@ module "compute" {
 module "network" {
   source  = "./modules/network"
   subnets = var.subnets
-
+  tf_vpc = var.tf_vpc
+  route_table = var.route_table
+  ingress_cidr_blocks = var.ingress_cidr_blocks
+  ingress_rules = var.ingress_rules
+  egress_cidr_blocks = var.egress_cidr_blocks
+  internet_gateway_tags = var.internet_gateway_tags
+  egress_rules = var.egress_rule
+  rules = var.rules
+  security_group_tag = var.security_group_tag
 }
