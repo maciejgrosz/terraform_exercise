ac4c3f95440c33cc7459f526a260b165dd096d38
Maciej Groszyk <maciej.groszyk8@gmail.com>
2022-10-06 15:04:07 +0200

    add modules

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@
 
 module "compute" {
     source = "./modules/compute"
-    # aws_instances = var.aws_instances
+    aws_instances = var.aws_instances
 
     subnets_list = [module.network.subnet_a_id, module.network.subnet_b_id]
     vpc_id = module.network.vpc_id
--- vars.tf
+++ vars.tf
@@ -2,3 +2,13 @@ variable "AWS_REGION" {
   default = "eu-west-2"
 }
 
+variable "aws_instances" {
+  type = map(object({
+    ami           = string
+    instance_type = string
+    start_script  = string
+    tags          = map(any)
+    key_name      = string
+  }))
+  description = "AWS instances"
+}
